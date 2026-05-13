5a5027443a7455acddfa95ab115e90ecb38ab086
Krishna Gupta <krishnagupta30mar@gmail.com>
2026-05-13 16:33:56 +0530

    Fix: Add shutterstock.com to remotePatterns in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -24,6 +24,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'encrypted-tbn0.gstatic.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.shutterstock.com',
+      },
     ],
   },
 };
