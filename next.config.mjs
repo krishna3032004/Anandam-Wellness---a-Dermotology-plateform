bcfe6b21ce2c878329657e7885e3131eb26f1745
Krishna Gupta <krishnagupta30mar@gmail.com>
2026-05-13 16:37:55 +0530

    Fix: Add media.istockphoto.com to remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -28,6 +28,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'www.shutterstock.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'media.istockphoto.com',
+      },
     ],
   },
 };
