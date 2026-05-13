dd9cd859d36e9adab2be862818828cf12a552c67
Krishna Gupta <krishnagupta30mar@gmail.com>
2026-05-13 20:13:37 +0530

    update

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -29,6 +29,42 @@
 // };
 
 // import scrollbarHide from "tailwind-scrollbar-hide";
+// import scrollbar from "tailwind-scrollbar";
+
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     './app/**/*.{js,ts,jsx,tsx}',
+//     './pages/**/*.{js,ts,jsx,tsx}',
+//     './components/**/*.{js,ts,jsx,tsx}',
+//   ],
+
+//   safelist: [
+//     "hidden",
+//     "block",
+//     "md:hidden",
+//     "md:block",
+//     "text-white",
+//     "text-blue-500",
+//   ],
+
+//   theme: {
+//     extend: {
+//       colors: {
+//         background: "var(--background)",
+//         foreground: "var(--foreground)",
+//       },
+//     },
+//   },
+
+//   plugins: [scrollbar],
+// };
+
+
+
+
+
+
 import scrollbar from "tailwind-scrollbar";
 
 /** @type {import('tailwindcss').Config} */
@@ -40,12 +76,16 @@ export default {
   ],
 
   safelist: [
-    "hidden",
-    "block",
-    "md:hidden",
-    "md:block",
-    "text-white",
-    "text-blue-500",
+    "pl-4",
+    "pr-6",
+    "space-x-6",
+    "p-3",
+    "py-4",
+    "px-5",
+    "rounded-full",
+    "flex",
+    "items-center",
+    "justify-between",
   ],
 
   theme: {
